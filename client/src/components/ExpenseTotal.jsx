0e424cf272e0c514a1d0bc61121faa185a5a170d
Ray Thomas <rthomas5678@gmail.com>
2025-01-16 15:56:16 -0700

    added notes on expense and income summary:

--- client/src/components/ExpenseTotal.jsx
+++ client/src/components/ExpenseTotal.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react'
 
+//Creating a function that takes expenses in as props
+//Expenses is defined in Home.jsx when the componenent is added into the page
 export default function ExpenseTotal({ expenses }) {
+  //Expenses summary is going to take all the expenses gathered on Home.jsx and add them together for a total
+  //.reduce() is a way to add up a total of an array
  const expenseSummary = expenses.reduce((total, expense) => total + expense.amount, 0);
 
+ //Can change, but this is decalring the Expense Total, and then adding in the expenseSummary whcih added all the values
+ //Output should look like "Expense Summary: $1200" or however much the total vlaue of the array comes out to
   return (
     <div>
         <h1>Expense Total: {expenseSummary}</h1>
--- client/src/components/IncomeTotal.jsx
+++ client/src/components/IncomeTotal.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react'
 
+//Creating a function that takes incomes in as props
+//Incomes is defined in Home.jsx when the componenent is added into the page
 export default function IncomeTotal({ incomes }) {
+  //Income summary is going to take all the income gathered on Home.jsx and add them together for a total
+  //.reduce() is a way to add up a total of an array
  const incomeSummary = incomes.reduce((total, income) => total + income.amount, 0);
 
+  //Can change, but this is decalring the Income Total, and then adding in the incomeSummary whcih added all the values
+ //Output should look like "Income Summary: $900" or however much the total vlaue of the array comes out to
   return (
     <div>
         <h1>Income Total: {incomeSummary}</h1>

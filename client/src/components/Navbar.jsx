e55a70db39662bfcb86d19755a3c39ff9fbf888a
Ray Thomas <rthomas5678@gmail.com>
2025-01-21 17:12:05 -0700

    added notes to pages and components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,13 +3,19 @@ import { useAuthContext } from '../hooks/useAuthContext'
 import { useLogout } from '../hooks/useLogout'
 
 export default function Navbar() {
+    //Grab user from authContext to use for changing navbar based on user status
     const { user } = useAuthContext()
+    //Grab logout to be used in navbar when a user is active
     const { logout } = useLogout()
-
+    //Create a funcation that logout out the user when submitted
     const handleSubmit = async () => {
         logout()
     }
+
   return (
+    //Test user status and change the navbar based on if there is a user active
+    //If there is a user logged in, the navbar will only show an h3 that says logout
+    //If there is now user, the navbar will show a link to the login and sign up pages
     <div className="navbar flex justify-between px-40 py-6">
         <Link to={'/'}>
             <h3 className="logo">Budget Tracker</h3>

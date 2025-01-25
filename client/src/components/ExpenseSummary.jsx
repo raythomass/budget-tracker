fed451a9b0d9bb8f4c26588e7c5f04ecb3bd3ca9
Ray Thomas <rthomas5678@gmail.com>
2025-01-24 20:18:17 -0700

    debugging invalid time date when after creating expense

--- client/src/components/ExpenseSummary.jsx
+++ client/src/components/ExpenseSummary.jsx
@@ -1,11 +1,41 @@
-import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { toast } from 'react-hot-toast'
+import { useAuthContext } from '../hooks/useAuthContext'
+import { useExpenseContext } from '../hooks/useExpenseContext'
+import { useState } from 'react'
 
 //"Expenses" is taken in to be be used as stated in the homepage 
 //Create a div inside the expenses.map so each user income will generate in the layout created
 export default function ExpenseSummary({ expenses }) {
+  const { user } = useAuthContext()
+  const { dispatch } = useExpenseContext()
+  const [error, setError] = useState(null)
+
+  const handleClick = async () => {
+    const expenseId = expenses._id
+    const response = await fetch(`http://localhost:3001/api/expenses/${expenseId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+  
+    })
+    const json = await response.json()
+
+    if(!response.ok) {
+      setError(json.error)
+      toast.error(error)
+    } 
+    if(response.ok) {
+      toast.success('Expense Deleted')
+      dispatch({type: "DELETE_EXPENSE", payload: json})
+    }
+
+   }
+
+
 
   return (
     <div className='expense-summary'>
@@ -16,7 +46,7 @@ export default function ExpenseSummary({ expenses }) {
             </div>
             <div className='flex items-center gap-4'>
               <h4 className='summary-red'>${expenses.amount}</h4>
-              <FontAwesomeIcon className='trash-icon' icon={faTrash} />
+              <FontAwesomeIcon onClick={handleClick} className='trash-icon' icon={faTrash} />
             </div>
           </div>
     </div>
@@ -24,10 +54,3 @@ export default function ExpenseSummary({ expenses }) {
 }
 
 
-// const handleSubmit = async () => {
-//   const expenseId = expenses._id
-//   const response = await fetch(`http://localhost:3001/api/expenses/${expenseId}`, {
-//     method: 'DELETE'
-
-//   })
-//  }

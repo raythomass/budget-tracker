19d954e98a906163730e1859012571cd8c36b1cc
Ray Thomas <rthomas5678@gmail.com>
2025-01-27 20:58:45 -0700

    added total money difference

--- client/src/components/ExpenseTotal.jsx
+++ client/src/components/ExpenseTotal.jsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react'
 export default function ExpenseTotal({ expenses }) {
   //Expenses summary is going to take all the expenses gathered on Home.jsx and add them together for a total
   //.reduce() is a way to add up a total of an array
- const expenseSummary = expenses.reduce((total, expense) => total + expense.amount, 0);
+ const expenseSummary = expenses && expenses.reduce((total, expense) => total + expense.amount, 0);
 
  //Can change, but this is decalring the Expense Total, and then adding in the expenseSummary whcih added all the values
  //Output should look like "Expense Summary: $1200" or however much the total vlaue of the array comes out to

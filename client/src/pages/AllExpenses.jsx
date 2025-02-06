88aff11fa6d71875f7e19ad373bc28347cf0844d
Ray Thomas <rthomas5678@gmail.com>
2025-02-05 22:37:48 -0700

    added all expenses

--- client/src/pages/AllExpenses.jsx
+++ client/src/pages/AllExpenses.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
 
+
+//Try a state to equal the data from the expenses array
+//The state should be uodated already and it shouldnt change from this page so the state should always update when this page is called
 export default function AllExpenses() {
+  const { user } = useAuthContext()
+  const [expenseData, setExpenseData] = useState([])
+
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      const response = await fetch ('http://localhost:3001/api/expenses/', {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      })
+      const json = await response.json()
+      console.log(json.data)
+
+      if(response) {
+        setExpenseData(json.data)
+      }
+    }
+
+    if(user) {
+      fetchExpenses()
+    }
+  }, [user])
+
   return (
-    <div>AllExpenses</div>
+    <div>
+      {expenseData && expenseData.map((expense) => (
+        <p key={expense._id}>{expense.description} {expense.amount}</p>
+      ))}
+    </div>
   )
 }
